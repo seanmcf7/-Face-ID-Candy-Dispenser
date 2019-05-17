7e335e4dbdd9ec5260e3977ea5229459095c518d
seanmcf7 <seanmcf@iastate.edu>
2019-05-16 22:25:03 -0500

    Reads input from pi camera

--- pi_recog.py
+++ pi_recog.py
@@ -2,6 +2,9 @@ import face_recognition as fc
 import cv2
 import numpy as np
 import pickle
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
 class Person:
     def __init__(self, person, status=True):
@@ -15,8 +18,14 @@ f = open("people.pickle", "rb")
 People = pickle.load(f)
 f.close()
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+# Camera
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 2
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# Give camera time to warm up
+time.sleep(0.1)
 
 # Initialize some variables
 face_locations = []
@@ -25,6 +34,7 @@ face_names = []
 process_this_frame = True
 
 while True:
+    '''
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
@@ -33,12 +43,16 @@ while True:
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
+    '''
 
     # Only process every other frame of video to save time
-    if process_this_frame:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        cv_image = frame.array
+        image = cv_image[:, :, ::-1]
+        
         # Find all the faces and face encodings in the current frame of video
-        face_locations = fc.face_locations(rgb_small_frame)
-        face_encodings = fc.face_encodings(rgb_small_frame, face_locations)
+        face_locations = fc.face_locations(image)
+        face_encodings = fc.face_encodings(image, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
@@ -58,32 +72,35 @@ while True:
                 name = People[best_match_index].name
 
             face_names.append(name)
+        rawCapture.truncate(0)
 
-    process_this_frame = not process_this_frame
+    # process_this_frame = not process_this_frame
 
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            '''
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            '''
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(cv_image, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(cv_image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(cv_image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+        # Display the resulting image
+        cv2.imshow('Video', cv_image)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 # Release handle to the webcam
 video_capture.release()
